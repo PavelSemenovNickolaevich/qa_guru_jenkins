75495b627dc181e7c2e850086feed85c9764934e
Pavel Semenov <PNSemenov@sberbank.ru>
2021-10-08 19:09:08 +0300

    added properties

--- src/test/java/demoqa/automation_practice_form/tests/TestBase.java
+++ src/test/java/demoqa/automation_practice_form/tests/TestBase.java
@@ -3,14 +3,23 @@ package demoqa.automation_practice_form.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import demoqa.automation_practice_form.config.CredentialsConfig;
 import demoqa.automation_practice_form.helpers.Attach;
-import demoqa.automation_practice_form.pages.RegistrationPage;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static java.lang.String.format;
+
 public class TestBase {
+    static CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    static String loginForSelenoid = credentials.login();
+    static String passwordForSelenoid = credentials.password();
+
 
     @BeforeAll
     static void beforeAll() {
@@ -22,7 +31,7 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = format("https://%s:%s@selenoid.autotests.cloud/wd/hub/", loginForSelenoid, passwordForSelenoid);
     }
 
     @AfterEach
